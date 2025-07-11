7a27397bf4c7198c246351533fad0b73fe98b535
Nguyen Huy Hoang <nguyenhuyhoang09.vn@gmail.com>
2025-07-11 16:19:12 +0700

    Fix(api): fix no body for request

--- src/main/java/com/defi/search/vertx/SearchApi.java
+++ src/main/java/com/defi/search/vertx/SearchApi.java
@@ -3,6 +3,7 @@ package com.defi.search.vertx;
 import com.defi.common.vertx.HttpApi;
 import com.defi.search.SearchHandler;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -14,21 +15,27 @@ public class SearchApi {
 
     private static void searchApi(Router router) {
         router.post("/admin/api/search/by-date-range")
-                        .handler(HttpApi.handleAsync(SearchHandler::handleFindByDateRange));
+                .handler(BodyHandler.create())
+                .handler(HttpApi.handleAsync(SearchHandler::handleFindByDateRange));
 
         router.post("/admin/api/search/user-by-type")
+                .handler(BodyHandler.create())
                 .handler(HttpApi.handleAsync(SearchHandler::handleFindUserByType));
 
         router.post("/admin/api/search/log-by-subject-id")
+                .handler(BodyHandler.create())
                 .handler(HttpApi.handleAsync(SearchHandler::handleFindEventLogBySubjectId));
 
         router.post("/admin/api/search/count-by-target")
+                .handler(BodyHandler.create())
                 .handler(HttpApi.handleAsync(SearchHandler::handleCountEventsByTarget));
 
         router.post("/admin/api/search/count-by-type")
+                .handler(BodyHandler.create())
                 .handler(HttpApi.handleAsync(SearchHandler::handleCountEventsByType));
 
         router.post("/admin/api/search/top-users")
+                .handler(BodyHandler.create())
                 .handler(HttpApi.handleAsync(SearchHandler::handleFindTopUsersByEventCount));
 
         log.info("Search API endpoints configured successfully");
